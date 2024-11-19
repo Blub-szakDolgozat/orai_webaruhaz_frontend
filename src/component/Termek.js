8c8c49780dd04092e9b4278968576e096ce1853d
DDorka02 <dobszaydorka@gmail.com>
2024-11-19 11:14:10 +0100

    javítás

---
+++ src/component/Termek.js
@@ -0,0 +1,31 @@
+import React, { useContext } from 'react'
+import { KosarContext } from '../contexts/KosarContext'
+
+
+export default function Termek(props) {
+
+  const { termekLista, setTermekLista } = useContext(ApiContext);
+
+  const torles = () => {
+    const listaFrissitese = termekLista.filter(item => item.id !== props.obj.id);
+    setTermekLista(listaFrissitese); 
+  };
+
+    const {kosarba} =useContext(KosarContext)
+    function kattintas() {
+        kosarba(props.adat)        
+    }
+
+  return (
+    <tr>
+      <td>{props.adat.cim}</td>
+        <td>{props.adat.leiras}</td>
+        <td>{props.adat.ar}</td>
+        <td>{props.adat.db}</td>
+        <td><button type="button" class="btn btn-primary" onClick={()=>kattintas()}>
+          Kosárba
+        </button></td>
+      </tr>
+      
+  )
+}
