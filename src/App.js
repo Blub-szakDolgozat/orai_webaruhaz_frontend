5faa2311ca204d8502cd13684961f08cf66289be
DDorka02 <dobszaydorka@gmail.com>
2024-11-19 09:56:16 +0100

    app js-be termékek

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Termek from './component/Termek';import Termekek from './component/Termekek';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Termékek: </h1>
-        <Ter
+        <Termekek />
       </header>
     </div>
   );
