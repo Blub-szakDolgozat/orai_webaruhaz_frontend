bfcac5298c11f716a0a03a430e177936e412423b
DDorka02 <dobszaydorka@gmail.com>
2024-11-19 10:59:25 +0100

    koasrContext létrehozása

--- src/component/Termek.js
+++ src/component/Termek.js
@@ -1,13 +1,19 @@
 import React from 'react'
 
 export default function Termek(props) {
+    const {kosarba} =useContext(KosarContext)
+    function kattintas() {
+        kosarba(props.adat)        
+    }
   return (
     <tr>
       <td>{props.adat.cim}</td>
         <td>{props.adat.leiras}</td>
         <td>{props.adat.ar}</td>
         <td>{props.adat.db}</td>
-        <td></td>
+        <td><button type="button" class="btn btn-primary" onClick={()=>kattintas()}>
+          Kosárba
+        </button></td>
       </tr>
       
   )
