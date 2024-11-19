2fa2aaa53778ec2ff7663c5adba7ebe0c0bb63be
DDorka02 <dobszaydorka@gmail.com>
2024-11-19 09:53:37 +0100

    Termékek létrehozása

--- src/component/Termek.js
+++ src/component/Termek.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 export default function Termek() {
   return (
-    <div>
-      <></>
-    </div>
+    <tr>
+      <td>{props.adat.cím}</td>
+        <td>{props.adat.leiras}</td>
+        <td>{props.adat.ar}</td>
+        <td>{props.adat.db}</td>
+      </tr>
+      
   )
 }
--- src/component/Termekek.js
+++ src/component/Termekek.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import Termek from './Termek'
 
 export default function Termekek() {
   return (
     <div>
-      
+      <table>
+        <th>Cím</th>
+        <th>Leírás</th>
+        <th>Ár</th>
+        <th>Db</th>
+      {
+        props.adminLista.map((element,i)=>{
+            return <Termek adat={element} key={element.id} />
+        }
+        )      
+        }
+
+    </table>
     </div>
   )
 }
