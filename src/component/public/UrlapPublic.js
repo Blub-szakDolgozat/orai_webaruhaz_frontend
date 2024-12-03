4c13988be58b046b418bd560d9c8510571cb4682
horvathnauzika <naihorvath@gmail.com>
2024-12-03 10:20:20 +0100

    kjdbgcv

--- src/component/public/UrlapPublic.js
+++ src/component/public/UrlapPublic.js
@@ -66,6 +66,8 @@ export default function UrlapPublic(){
                 placeholder="Írja le a terméket"
               />
             </Form.Group>
+
+          
     
             <Form.Group className="mb-3" controlId="image">
               <Form.Label>A termék képe (URL)</Form.Label>
