e31adb1aa4c0330c3e959cef0774dd92ff37f82a
DDorka02 <dobszaydorka@gmail.com>
2024-12-03 09:01:55 +0100

    kosar context

--- src/contexts/KosarContext.js
+++ src/contexts/KosarContext.js
@@ -5,9 +5,21 @@ export const KosarContext = createContext("");
 export const KosarProvider = ({ children }) => {
     const [kosarlista, setkosarlista] = useState([]);
 
+    function kosarba(adat) {
+      const skosarLista = [...kosarlista];
+      const VanIlyen =  skosarLista.find((elem) => elem.id === adat.id);
+      if (VanIlyen === undefined) {
+      adat.db = 1;
+      skosarLista.push(adat);
+      } else {
+      VanIlyen.db++;
+      } 
+      setkosarlista([...skosarLista]);
+    }
+
 
   return (
-      <KosarContext.Provider value={{kosarlista}}>
+      <KosarContext.Provider value={{ kosarba, kosarlista}}>
       {children}
       </KosarContext.Provider>
   );
