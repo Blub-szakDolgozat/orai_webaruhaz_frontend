aae4ba86a71809188bf940d4e1844571823ee8b7
midnightcrow00 <orsolyaalbert1@gmail.com>
2024-12-03 09:29:48 +0100

    kép kiszedése

--- src/component/public/Kartya.js
+++ src/component/public/Kartya.js
@@ -12,17 +12,7 @@ export default function Kartya(props) {
   return (
     <Col xs={12} md={6} lg={4} className="mb-4">
       <Card className="shadow-lg border-0 rounded">
-        <Card.Img
-          variant="top"
-          src={props.adat.image}
-          alt={props.adat.cim}
-          style={{
-            width: "100%",
-            height: "200px",
-            objectFit: "cover",
-            borderRadius: "8px",
-          }}
-        />
+        
         <Card.Body>
           <Card.Title>{props.adat.cim}</Card.Title>
           <Card.Text>{props.adat.leiras}</Card.Text>
